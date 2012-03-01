39bb01703e87e26e430b168beca371e3225765b8
Vanzo <vanzo32@gmail.com>
2012-02-29 21:24:48 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quattro::Application.config.secret_token = '69f66966b72fdd6795674123576ff1dde9b03905e7bce534ce0807d8add7ad773361c388389f920d6bdc5f2caece45e8f6d96790dba0d1d64a33f83c371151af'
